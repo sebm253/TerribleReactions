fdc0097d1a2a6e18eb7e11ebfe3d6885bf40048c
mlnrDev <git@mlnr.dev>
2023-07-16 16:58:06 +0200

    bump disgo

--- bot.go
+++ bot.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"github.com/disgoorg/disgo"
 	"github.com/disgoorg/disgo/bot"
 	"github.com/disgoorg/disgo/cache"
-	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/events"
 	"github.com/disgoorg/disgo/gateway"
 	"github.com/disgoorg/log"
-	"github.com/disgoorg/snowflake/v2"
 	"os"
 	"os/signal"
 	"syscall"
@@ -23,11 +20,17 @@ func main() {
 
 	client, err := disgo.New(os.Getenv("TERRIBLE_REACTIONS_TOKEN"),
 		bot.WithGatewayConfigOpts(gateway.WithIntents(gateway.IntentGuildMessageReactions),
-			gateway.WithPresenceOpts(gateway.WithWatchingActivity("super reactions")),
-			gateway.WithEnableRawEvents(true)),
+			gateway.WithPresenceOpts(gateway.WithWatchingActivity("super reactions"))),
 		bot.WithCacheConfigOpts(cache.WithCaches(cache.FlagsNone)),
 		bot.WithEventListeners(&events.ListenerAdapter{
-			OnRaw: onRaw,
+			OnMessageReactionAdd: func(event *events.MessageReactionAdd) {
+				if event.Burst {
+					rest := event.Client().Rest()
+					if err := rest.RemoveUserReaction(event.ChannelID, event.MessageID, event.Emoji.Reaction(), event.UserID); err != nil {
+						log.Error("there was an error while removing a burst reaction: ", err)
+					}
+				}
+			},
 		}))
 	if err != nil {
 		log.Fatal("error while building disgo instance: ", err)
@@ -43,29 +46,4 @@ func main() {
 	s := make(chan os.Signal, 1)
 	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-s
-}
-
-func onRaw(event *events.Raw) {
-	if event.EventType != gateway.EventTypeMessageReactionAdd {
-		return
-	}
-	var payload ReactionPayload
-	if err := json.NewDecoder(event.Payload).Decode(&payload); err != nil {
-		log.Error("there was an error while decoding the payload: ", err)
-		return
-	}
-	if payload.Burst {
-		rest := event.Client().Rest()
-		if err := rest.RemoveUserReaction(payload.ChannelID, payload.MessageID, payload.Emoji.Reaction(), payload.UserID); err != nil {
-			log.Error("there was an error while removing a burst reaction: ", err)
-		}
-	}
-}
-
-type ReactionPayload struct {
-	UserID    snowflake.ID         `json:"user_id"`
-	MessageID snowflake.ID         `json:"message_id"`
-	ChannelID snowflake.ID         `json:"channel_id"`
-	Emoji     discord.PartialEmoji `json:"emoji"`
-	Burst     bool                 `json:"burst"`
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module terrible-reactions
 go 1.20
 
 require (
-	github.com/disgoorg/disgo v0.16.5
+	github.com/disgoorg/disgo v0.16.8-0.20230716144838-a5617470e5ea
 	github.com/disgoorg/log v1.2.0
-	github.com/disgoorg/snowflake/v2 v2.0.1
 )
 
 require (
 	github.com/disgoorg/json v1.1.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/sasha-s/go-csync v0.0.0-20210812194225-61421b77c44b // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
